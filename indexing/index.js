a49f621d17f25f9dfaf9e15f1da55df76164962e
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-10-03 22:15:18 +0200

    Great improvements to the indexing.

--- indexing/index.js
+++ indexing/index.js
@@ -1,33 +1,55 @@
 var settings = require('../settings.json');
-var titles = require('./titles').init(settings.elasticsearch, 'titles');
+var titles = require('./titles').init(settings.elasticsearch, 'books');
 
 (function() {
   var csv = require('csv');
   var fs = require('fs');
   var Promise = require("bluebird");
-  var progress = require('progress-stream');
 
   var parser = csv.parse({columns: true});
-
-  function insert(row) {
-    return Promise.resolve().then(function() {
-      if(row.type === 'book') {
-        return titles.insert('dbc', row.materiale_id, row.title);
-      }
-    });
-  }
 
   // Only start the chain of nexts on the first readable token.
   var parsing = false;
   var parsed = 0;
+  var skipped = 0;
   var datafile = __dirname+'/../../materialedata.csv';
 
+  var BATCH_SIZE = 100;
+  var rows = [];
   function next() {
     var row = parser.read();
-    console.log(parsed);
-    parsed++;
     if(row) {
-      return insert(row).then(next);
+      if(row.type === 'book') {
+        var doc = {
+          source: 'dbc',
+          title: row.title,
+          _id: 'dbc-'+row.materiale_id
+        };
+
+        if(row.dk5) {
+          try {
+            doc.dk5 = JSON.parse(row.dk5);
+          } catch(err) {
+            console.error(err);
+          }
+        }
+        
+        rows.push(doc);
+
+        if(rows.length >= BATCH_SIZE) {
+          console.log('Parsed:', parsed,
+                      'Skipped:', skipped,
+                      'Total:', parsed+skipped);
+          parsed += rows.length;
+          return titles.insertMany(rows).then(function() {
+            // Reset the rows buffer.
+            rows = [];
+          }).then(next);
+        }
+      } else {
+        skipped++;
+      }
+      return Promise.resolve().then(next);
     } else {
       return Promise.resolve(null);
     }
