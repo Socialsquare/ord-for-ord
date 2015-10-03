59fe9f2f20860d6ae07f36dcfe5d4d3217f3c215
Andreas Larsen <andreas@larsenwork.com>
2015-10-03 16:31:19 +0200

    welcome.js + style updates

--- client/components/welcome.js
+++ client/components/welcome.js
@@ -12,19 +12,28 @@ var WelcomeComponent = React.createClass({
     return (
       <div>
         <div className="container-fluid">
-          <div className="page-header">
-            <h1>Welcome to the game!</h1>
+          <div className="row">
+            <div className="col-xs-12">
+              <div className="page-header m-t-lg">
+                <h1>Welcome to the game!</h1>
+              </div>
+              <p>&#8216;In Other Words&#8217; is a multiplayer word game challenging
+              your inner wordsmith. Use words represented in as many book titles as
+              possible and challenge your friends in building new sentences.
+
+              In Other Words - have fun!</p>
+
+            </div>
           </div>
-          <p>&#8216;In Other Words&#8217; is a multiplayer word game challenging your inner wordsmith. Use words represented in as many book titles as possible and challenge your friends in building new sentences.
-
-          In Other Words - have fun!</p>
-
-
         </div>
         <footer className="footer">
           <div className="container-fluid">
-            <button type="button" className="btn btn-primary"
-              onClick={this.joinLobby}>Join Lobby</button>
+            <div className="row">
+              <div className="col-xs-6 col-xs-offset-3">
+                <button type="button" className="btn btn-primary btn-lg btn-block"
+                  onClick={this.joinLobby}>Join Lobby</button>
+              </div>
+            </div>
           </div>
         </footer>
       </div>
