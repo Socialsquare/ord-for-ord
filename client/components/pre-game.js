8f18d718f969377bb09ffb3c917d1af01d8cf948
Andreas Larsen <andreas@larsenwork.com>
2015-10-04 03:54:48 +0200

    words and styles

--- client/components/pre-game.js
+++ client/components/pre-game.js
@@ -29,12 +29,12 @@ var PreGameComponent = React.createClass({
     if (App.player().isJudge() === true) {
       title = 'Hej overdommer!';
       content = (
-        <div>
+        <div className="judge-pre">
           <form onSubmit={this.startRound}>
-            <input autoFocus="true" ref="word" />
+            <input autoFocus="true" ref="word" placeholder="Skriv det første ord." />
             <button className="submit-button" />
           </form>
-          <div className="style-select dropdown-toggle">
+          <div className="style-select dropdown-toggle m-t-md">
             <select id="category-select">
               <option selected disabled>Vælg en kategori</option>
               <option>1</option>
@@ -44,19 +44,18 @@ var PreGameComponent = React.createClass({
               <option>5</option>
             </select>
           </div>
-          <div className="m-t-lg">Som dommer har du altid det sidste ord!<br/><br/>
-          Væbnet med punktummer kan du til en hver tid afslutte en sætning, hvis
-          meningen udebliver.</div>
+          <div className="m-t-md">Som dommer kan du til en hver tid afslutte
+          sætning, når meningen udebliver.</div>
         </div>
       );
     } else {
       title = 'Hej Spiller!';
       content = (
-        <div>Spillet starter når overdommeren har defineret det første ord
+        <div>Spillet starter, når overdommeren har defineret det første ord
         og valgt en kategori.<br/><br/>
-        Når det er din tur skriver du et ord, der i sammenhæng med de forrige
+        Når det er din tur, skriver du et ord, der i sammenhæng med de forrige
         forsat er meningsgivende og som du mener vil indgå i flest bogtitler.<br/><br/>
-        Når det er din modspilleres tur har du mulighed for at udfordre dem ved
+        Når det er din modspilleres tur, har du mulighed for at udfordre dem ved
         at blokere ord og dermed stjæle deres points.<br/><br/>
         Spillet slutter efter (tid/runder) eller når overdommeren lader sætter
         et punktum.</div>
