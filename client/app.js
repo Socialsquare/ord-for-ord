04144fd9019ee2fcfa5c42be0bbd17655f1ae16d
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-10-04 14:26:21 +0200

    Removing debug data.

--- client/app.js
+++ client/app.js
@@ -37,60 +37,6 @@ var App = {
     var AppLayout = require('./app-layout'),
         PlayerCollection = require('./models/player');
 
-    /*
-    var game = require('./models/game');
-    game.set({
-      id: 'gae',
-      state: 'game-ended',
-      judgeId: '1',
-      currentPlayerId: '2'
-    });
-    game.players.add({ id: '1', color: 0 });
-    game.players.add({ id: '2', color: 1 });
-    game.players.add({ id: '3', color: 2 });
-    game.players.add({ id: '4', color: 3 });
-    game.players.add({ id: '5', color: 4 });
-    this.state = 'game-ended';
-    this.playerId = '2';
-
-    game.words.add({
-      word: 'Hej',
-      playerId: '2',
-      score: 20,
-      successfulClaims: []
-    });
-
-    game.words.add({
-      word: 'hvad',
-      playerId: '3',
-      score: 20,
-      successfulClaims: []
-    });
-
-    game.words.add({
-      word: 'laver',
-      playerId: '4',
-      score: 20,
-      successfulClaims: ['2', '3']
-    });
-
-    game.words.add({
-      word: 'du',
-      playerId: '5',
-      score: 20,
-      successfulClaims: []
-    });
-
-    game.words.add({
-      word: 'lige',
-      playerId: '2',
-      score: 20,
-      successfulClaims: ['4']
-    });
-    */
-
-    */
-
     React.render(<AppLayout />, document.body);
   }
 };
