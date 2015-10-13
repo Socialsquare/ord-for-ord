fa7d79351ca6311c6b3e904cfcb38dacb405977f
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-10-13 10:26:46 +0200

    List the whole sentence when the game ends.

--- client/components/game-ended.js
+++ client/components/game-ended.js
@@ -33,12 +33,23 @@ var GameEndedComponent = React.createClass({
       );
     }
 
+    var words = game.words.map(function(w) {
+      return w.get('word');
+    }).join(' ');
+
+    if(words.length > 0) {
+      var firstLetter = words[0].toUpperCase();
+      words = firstLetter + words.substr(1);
+    }
+
     return (
       <div className="panel">
         <div className="container-fluid">
           <div className="row">
             <div className="col-xs-12 m-t-lg">
               <h2>Spillet er slut!</h2>
+
+              <p>{words}</p>
 
               <ScoreComponent game={game} />
 
