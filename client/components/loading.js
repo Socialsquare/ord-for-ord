be7f055955885abdb5c47fc816c44b58d64a9491
Andreas Larsen <andreas@larsenwork.com>
2015-11-29 11:48:24 +0100

    book loading

--- client/components/loading.js
+++ client/components/loading.js
@@ -8,15 +8,20 @@ var LoadingComponent = React.createClass({
       <div className="panel">
         <div className="container-fluid">
           <div className="row">
-            <div className="col-xs-12">
-              <h1>Venter på bibliotikaren</h1>
-              <h2 className="text-center">
-                <span className="loading-dots">
-                  <span className="dot dot-1">&bull;</span>
-                  <span className="dot dot-2">&bull;</span>
-                  <span className="dot dot-3">&bull;</span>
-                </span>
-              </h2>
+            <div className="col-xs-12 text-center">
+              <h3>Venter på bibliotikaren</h3>
+              <p>Bogtitler hentes</p>
+            </div>
+            <div className="bookshelf_wrapper">
+              <ul className="books_list">
+                <li className="book_item first"></li>
+                <li className="book_item second"></li>
+                <li className="book_item third"></li>
+                <li className="book_item fourth"></li>
+                <li className="book_item fifth"></li>
+                <li className="book_item sixth"></li>
+              </ul>
+              <div className="shelf"></div>
             </div>
           </div>
         </div>
